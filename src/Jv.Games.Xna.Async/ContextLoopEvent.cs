e26a6702f407a10bb70eb8558bcb8f57f7db4c20
Joao Vitor P. Moraes <jvlppm@gmail.com>
2013-12-02 22:27:55 -0200

    ContextLoopEvent.

---
+++ src/Jv.Games.Xna.Async/ContextLoopEvent.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.Xna.Framework;
+using System;
+
+namespace Jv.Games.Xna.Async
+{
+    public class ContextLoopEventArgs<T> : GameLoopEventArgs
+        where T : GameLoopEventArgs
+    {
+        public ContextLoopEventArgs(SyncContext<T> context, GameTime gameTime)
+            : base(gameTime)
+        {
+            Context = context;
+        }
+
+        public SyncContext<T> Context { get; private set; }
+    }
+
+    public delegate void ContextLoopEventHandler<T>(object sender, ContextLoopEventArgs<T> e) where T : GameLoopEventArgs;
+}
