68788c1dd02671fa68de0354d1c9c8e7b01ab5c9
Joao Vitor P. Moraes <jvlppm@gmail.com>
2013-12-23 13:18:57 -0200

    Created AsyncGameComponent.

--- src/Jv.Games.Xna.Async/AsyncGameComponent.cs
+++ src/Jv.Games.Xna.Async/AsyncGameComponent.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Jv.Games.Xna.Async.Operations;
+using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,42 +7,109 @@ using System.Text;
 
 namespace Jv.Games.Xna.Async
 {
-    sealed class AsyncGameComponent : DrawableGameComponent
+    public class AsyncGameComponent<T> : IGameComponent, IDrawable, IUpdateable
     {
-        public readonly DrawableGameComponent Component;
+        #region Attributes
         public readonly AsyncContext DrawContext, UpdateContext;
 
-        public AsyncGameComponent(Game game, DrawableGameComponent component)
-            : base(game)
+        int _drawOrder, _updateOrder;
+        bool _visible, _enabled;
+        #endregion
+
+        #region Properties
+        public int DrawOrder
         {
-            Component = component;
+            get { return _drawOrder; }
+            set
+            {
+                if (_drawOrder == value)
+                    return;
+
+                _drawOrder = value;
+
+                if (DrawOrderChanged != null)
+                    DrawOrderChanged(this, EventArgs.Empty);
+            }
+        }
+
+        public int UpdateOrder
+        {
+            get { return _updateOrder; }
+            set
+            {
+                if (_updateOrder == value)
+                    return;
+
+                _updateOrder = value;
+
+                if (UpdateOrderChanged != null)
+                    UpdateOrderChanged(this, EventArgs.Empty);
+            }
+        }
+
+        public bool Visible
+        {
+            get { return _visible; }
+            set
+            {
+                if (_visible == value)
+                    return;
+
+                _visible = value;
+
+                if (VisibleChanged != null)
+                    VisibleChanged(this, EventArgs.Empty);
+            }
+        }
+
+        public bool Enabled
+        {
+            get { return _enabled; }
+            set
+            {
+                if (_enabled == value)
+                    return;
+
+                _enabled = value;
+
+                if (EnabledChanged != null)
+                    EnabledChanged(this, EventArgs.Empty);
+            }
+        }
+        #endregion
+
+        #region Events
+        public event EventHandler<EventArgs> DrawOrderChanged;
+        public event EventHandler<EventArgs> VisibleChanged;
+        public event EventHandler<EventArgs> EnabledChanged;
+        public event EventHandler<EventArgs> UpdateOrderChanged;
+        #endregion
+
+        public AsyncGameComponent(Game game)
+        {
             DrawContext = new AsyncContext();
             UpdateContext = new AsyncContext();
         }
 
-        public override void Draw(GameTime gameTime)
+        public void Initialize()
         {
-            DrawContext.Post(Component.Draw);
-            DrawContext.Update(gameTime);
+            UpdateContext.Send((Action<AsyncContext>)Initialize);
         }
 
-        public override void Update(GameTime gameTime)
+        public void Draw(GameTime gameTime)
         {
-            UpdateContext.Post(Component.Update);
-            UpdateContext.Update(gameTime);
+            DrawContext.Send(Draw, gameTime);
         }
 
-        protected override void Dispose(bool disposing)
+        public void Update(GameTime gameTime)
         {
-            if(disposing)
-                Component.Dispose();
-            base.Dispose(disposing);
+            UpdateContext.Send(Update, gameTime);
         }
 
-        public override void Initialize()
-        {
-            Component.Initialize();
-            base.Initialize();
-        }
+        protected virtual void Initialize(AsyncContext context) { }
+
+        protected virtual void Draw(AsyncContext context, GameTime gameTime) { }
+
+        protected virtual void Update(AsyncContext context, GameTime gameTime) { }
     }
 }
--- src/Jv.Games.Xna.Async/ContextExtensions.cs
+++ src/Jv.Games.Xna.Async/ContextExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,24 @@ namespace Jv.Games.Xna.Async
             var oldContext = TaskAwaiter.CurrentContext;
             TaskAwaiter.CurrentContext = context;
             return Disposable.Create(() => TaskAwaiter.CurrentContext = oldContext);
+        }
+
+        public static void Send(this AsyncContext context, System.Action action)
+        {
+            using (context.Activate())
+                action();
+        }
+
+        public static void Send(this AsyncContext context, System.Action<AsyncContext> action)
+        {
+            using (context.Activate())
+                action(context);
+        }
+
+        public static void Send(this AsyncContext context, System.Action<AsyncContext, GameTime> action, GameTime gameTime)
+        {
+            using (context.Activate())
+                action(context, gameTime);
         }
     }
 }
