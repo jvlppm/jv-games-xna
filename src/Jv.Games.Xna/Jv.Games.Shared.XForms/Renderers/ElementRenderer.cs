7fdbd8a29f22f1f73d96dc549b355462fe10bb14
Joao Vitor P. Moraes <joao.moraes@lactec.org.br>
2014-09-01 13:34:30 -0300

    Created RendererBase.

--- src/Jv.Games.Xna/Jv.Games.Shared.XForms/Renderers/ElementRenderer.cs
+++ src/Jv.Games.Xna/Jv.Games.Shared.XForms/Renderers/ElementRenderer.cs
@@ -6,34 +6,16 @@ namespace Jv.Games.Xna.XForms.Renderers
     using System;
     using System.Linq;
     using System.Collections.Generic;
-    using System.Text;
     using Xamarin.Forms;
-    using Microsoft.Xna.Framework.Graphics;
-    using Microsoft.Xna.Framework;
 
-    public class ElementRenderer<TModel> : BindableObject, IControlRenderer
+    public class ElementRenderer<TModel> : RendererBase
         where TModel : Element
     {
-        #region Bindable Properties
-        static BindableProperty ModelProperty = BindableProperty.Create<ElementRenderer<TModel>, TModel>(p => p.Model, null, propertyChanged: ModelPropertyChanged);
-        public TModel Model
+        #region Properties
+        public new TModel Model
         {
-            get { return (TModel)GetValue(ModelProperty); }
-            set { SetValue(ModelProperty, value); }
-        }
-        static void ModelPropertyChanged(BindableObject bindable, TModel oldValue, TModel newValue)
-        {
-            var rend = bindable as ElementRenderer<TModel>;
-            if (rend == null)
-                return;
-
-            var oldModel = oldValue as TModel;
-            if (oldModel != null)
-                rend.UnloadModel(oldModel);
-
-            var newModel = newValue as TModel;
-            if (newModel != null)
-                rend.LoadModel(newModel);
+            get { return (TModel)base.Model; }
+            set { base.Model = value; }
         }
         #endregion
 
@@ -51,6 +33,17 @@ namespace Jv.Games.Xna.XForms.Renderers
         #endregion
 
         #region Methods
+        protected override void OnModelChanged(Element oldValue, Element newValue)
+        {
+            base.OnModelChanged(oldValue, newValue);
+
+            if (oldValue != null)
+                UnloadModel((TModel)oldValue);
+
+            if (newValue != null)
+                LoadModel((TModel)newValue);
+        }
+
         protected virtual void LoadModel(TModel model)
         {
             model.PropertyChanged += OnPropertyChanged;
@@ -77,43 +70,6 @@ namespace Jv.Games.Xna.XForms.Renderers
         {
             foreach (var property in HandledProperties[e.PropertyName])
                 Handlers[property].FirstOrDefault(handle => handle(property));
-        }
-        #endregion
-
-        #region IControlRenderer
-        Element IControlRenderer.Model
-        {
-            get { return Model; }
-            set { Model = (TModel)value; }
-        }
-
-        public virtual void Initialize(Microsoft.Xna.Framework.Game game)
-        {
-        }
-
-        public virtual void Draw(Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch, Microsoft.Xna.Framework.GameTime gameTime)
-        {
-        }
-
-        public virtual void Update(Microsoft.Xna.Framework.GameTime gameTime)
-        {
-        }
-
-        void IControlRenderer.Draw(Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch, GameTime gameTime)
-        {
-            BeginDraw(spriteBatch);
-            Draw(spriteBatch, gameTime);
-            EndDraw(spriteBatch);
-        }
-
-        protected virtual void BeginDraw(Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch)
-        {
-            spriteBatch.Begin(SpriteSortMode.Deferred, null);
-        }
-
-        protected virtual void EndDraw(Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch)
-        {
-            spriteBatch.End();
         }
         #endregion
     }
---
+++ src/Jv.Games.Xna/Jv.Games.Shared.XForms/Renderers/RendererBase.cs
@@ -0,0 +1,60 @@
+﻿namespace Jv.Games.Xna.XForms.Renderers
+{
+    using Xamarin.Forms;
+    using Microsoft.Xna.Framework.Graphics;
+    using Microsoft.Xna.Framework;
+
+    public class RendererBase : BindableObject, IControlRenderer
+    {
+        #region Model
+        static BindableProperty ModelProperty = BindableProperty.Create<RendererBase, Element>(p => p.Model, null, propertyChanged: ModelPropertyChanged);
+        public Element Model
+        {
+            get { return (Element)GetValue(ModelProperty); }
+            set { SetValue(ModelProperty, value); }
+        }
+        static void ModelPropertyChanged(BindableObject bindable, Element oldValue, Element newValue)
+        {
+            var rend = bindable as RendererBase;
+            if (rend != null)
+                rend.OnModelChanged(oldValue, newValue);
+        }
+
+        protected virtual void OnModelChanged(Element oldValue, Element newValue)
+        {
+        }
+        #endregion
+
+        #region IControlRenderer
+        public virtual void Initialize(Game game)
+        {
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+        }
+
+        void IControlRenderer.Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            BeginDraw(spriteBatch);
+            Draw(spriteBatch, gameTime);
+            EndDraw(spriteBatch);
+        }
+
+        protected virtual void BeginDraw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin(SpriteSortMode.Deferred, null);
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+        }
+
+        protected virtual void EndDraw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.End();
+        }
+        #endregion
+    }
+}
+
