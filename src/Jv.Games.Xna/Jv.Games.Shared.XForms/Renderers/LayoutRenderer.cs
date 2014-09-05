1c1330c96268225032e3745d51209afa45ce433e
Joao Vitor P. Moraes <jvlppm@gmail.com>
2014-09-04 23:07:09 -0300

    WIP: StackLayout.

---
+++ src/Jv.Games.Xna/Jv.Games.Shared.XForms/Renderers/LayoutRenderer.cs
@@ -0,0 +1,83 @@
+﻿[assembly: Jv.Games.Xna.XForms.ExportRenderer(
+    typeof(Xamarin.Forms.Layout),
+    typeof(Jv.Games.Xna.XForms.Renderers.LayoutRenderer))]
+namespace Jv.Games.Xna.XForms.Renderers
+{
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Content;
+    using Microsoft.Xna.Framework.Graphics;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Xamarin.Forms;
+
+    public class LayoutRenderer : LayoutRenderer<Layout>
+    {
+    }
+
+    public class LayoutRenderer<TModel> : ViewRenderer<TModel>
+        where TModel : Layout
+    {
+        protected Dictionary<Element, IControlRenderer> ChildrenRenderers;
+
+        public LayoutRenderer()
+        {
+            ChildrenRenderers = new Dictionary<Element, IControlRenderer>();
+        }
+
+        public override void Initialize(Game game)
+        {
+            foreach (var c in ChildrenRenderers.Values)
+                c.Initialize(game);
+            base.Initialize(game);
+        }
+
+        protected override void LoadModel(TModel model)
+        {
+            foreach (var c in Model.LogicalChildren)
+                Model_ChildAdded(this, new ElementEventArgs(c));
+            Model.ChildAdded += Model_ChildAdded;
+            Model.ChildRemoved += Model_ChildRemoved;
+            base.LoadModel(model);
+        }
+
+        protected override void UnloadModel(TModel model)
+        {
+            ChildrenRenderers.Clear();
+            Model.ChildAdded -= Model_ChildAdded;
+            Model.ChildRemoved -= Model_ChildRemoved;
+            base.UnloadModel(model);
+        }
+
+        void Model_ChildRemoved(object sender, ElementEventArgs e)
+        {
+            ChildrenRenderers.Remove(e.Element);
+        }
+
+        void Model_ChildAdded(object sender, ElementEventArgs e)
+        {
+            ChildrenRenderers[e.Element] = RendererFactory.Create(e.Element);
+            if (Game != null)
+                ChildrenRenderers[e.Element].Initialize(Game);
+        }
+
+        protected override void BeginDraw(SpriteBatch spriteBatch)
+        {
+        }
+
+        protected override void EndDraw(SpriteBatch spriteBatch)
+        {
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            foreach (var it in ChildrenRenderers)
+                it.Value.Update(gameTime);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            foreach (var it in ChildrenRenderers)
+                it.Value.Draw(spriteBatch, gameTime);
+        }
+    }
+}
---
+++ src/Jv.Games.Xna/Jv.Games.Shared.XForms/Renderers/StackLayoutRenderer.cs
@@ -0,0 +1,123 @@
+﻿[assembly: Jv.Games.Xna.XForms.ExportRenderer(
+    typeof(Xamarin.Forms.StackLayout),
+    typeof(Jv.Games.Xna.XForms.Renderers.StackLayoutRenderer))]
+namespace Jv.Games.Xna.XForms.Renderers
+{
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Content;
+    using Microsoft.Xna.Framework.Graphics;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Xamarin.Forms;
+
+    public class StackLayoutRenderer : StackLayoutRenderer<StackLayout>
+    {
+    }
+
+    public class StackLayoutRenderer<TModel> : LayoutRenderer<TModel>
+        where TModel : StackLayout
+    {
+        public IEnumerable<IControlRenderer> Children
+        {
+            get
+            {
+                if (Model == null)
+                    return Enumerable.Empty<IControlRenderer>();
+
+                return from c in Model.Children
+                       where c.IsVisible
+                       select ChildrenRenderers[c];
+            }
+        }
+
+        public StackLayoutRenderer()
+        {
+            HandleProperty(StackLayout.SpacingProperty, HandleMeasureChange);
+            HandleProperty(StackLayout.OrientationProperty, HandleMeasureChange);
+        }
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            Size measuredSize = Size.Zero;
+            if (Model.Orientation == StackOrientation.Horizontal)
+            {
+                foreach (var child in Children)
+                {
+                    child.Measure(availableSize);
+                    measuredSize.Width += child.MeasuredSize.Width + Model.Spacing;
+                    availableSize.Width -= child.MeasuredSize.Width;
+                    if (measuredSize.Height < child.MeasuredSize.Height)
+                        measuredSize.Height = child.MeasuredSize.Height;
+                }
+                measuredSize.Width -= Model.Spacing;
+            }
+            else
+            {
+                foreach (var child in Children)
+                {
+                    child.Measure(availableSize);
+                    measuredSize.Height += child.MeasuredSize.Height + Model.Spacing;
+                    availableSize.Height -= child.MeasuredSize.Height;
+                    if (measuredSize.Width < child.MeasuredSize.Width)
+                        measuredSize.Width = child.MeasuredSize.Width;
+                }
+                measuredSize.Height -= Model.Spacing;
+            }
+
+            return measuredSize;
+        }
+
+        protected override Xamarin.Forms.Rectangle ArrangeOverride(Xamarin.Forms.Rectangle finalRect)
+        {
+            if (Model.Orientation == StackOrientation.Horizontal)
+                return ArrangeHorizontally(ref finalRect);
+            return ArrangeVertically(ref finalRect);
+        }
+
+        private Xamarin.Forms.Rectangle ArrangeVertically(ref Xamarin.Forms.Rectangle finalRect)
+        {
+            var expandableChildrenCount = Model.Children.Count(c => c.VerticalOptions.Expands);
+            var extraSpace = finalRect.Height - MeasuredSize.Height;
+            var itemExtraSpace = expandableChildrenCount == 0 ? 0 : extraSpace / expandableChildrenCount;
+
+            double x = 0, y = 0;
+
+            foreach (var child in Model.Children)
+            {
+                var rend = ChildrenRenderers[child];
+                rend.Arrange(new Xamarin.Forms.Rectangle(
+                    x,
+                    y,
+                    finalRect.Width,
+                    rend.MeasuredSize.Height + (child.VerticalOptions.Expands ? itemExtraSpace : 0)
+                ));
+                y += rend.RenderArea.Height + Model.Spacing;
+            }
+
+            return new Xamarin.Forms.Rectangle(finalRect.X, finalRect.Y, x, y);
+        }
+
+        private Xamarin.Forms.Rectangle ArrangeHorizontally(ref Xamarin.Forms.Rectangle finalRect)
+        {
+            var expandableChildrenCount = Model.Children.Count(c => c.HorizontalOptions.Expands);
+            var extraSpace = finalRect.Width - MeasuredSize.Width;
+            var itemExtraSpace = expandableChildrenCount == 0 ? 0 : extraSpace / expandableChildrenCount;
+
+            double x = 0, y = 0;
+
+            foreach (var child in Model.Children)
+            {
+                var rend = ChildrenRenderers[child];
+                rend.Arrange(new Xamarin.Forms.Rectangle(
+                    x,
+                    y,
+                    rend.MeasuredSize.Width + (child.HorizontalOptions.Expands ? itemExtraSpace : 0),
+                    finalRect.Height
+                ));
+                x += rend.RenderArea.Width + Model.Spacing;
+            }
+
+            return new Xamarin.Forms.Rectangle(finalRect.X, finalRect.Y, x, y);
+        }
+    }
+}
