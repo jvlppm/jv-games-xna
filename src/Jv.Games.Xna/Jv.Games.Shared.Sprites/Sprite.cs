afc54180bcab60e3d19f1b2602a6e4164bbbf712
Joao Vitor Pietsiaki Moraes <joao.moraes@lactec.org.br>
2015-06-11 13:06:11 -0300

    GridSpriteSheet and LoopedAnimation.

--- src/Jv.Games.Xna/Jv.Games.Shared.Sprites/Sprite.cs
+++ src/Jv.Games.Xna/Jv.Games.Shared.Sprites/Sprite.cs
@@ -5,18 +5,44 @@
     using System;
     using System.Collections.Generic;
 
+    /// <summary>
+    /// Animated game Sprite.
+    /// </summary>
     public class Sprite : ICollection<Animation>
     {
         Dictionary<string, Animation> _animations;
 
+        /// <summary>
+        /// The color to tint the sprite. Use <c>Color.White</c> for full color with no tinting.
+        /// Default is <c>Color.White</c>.
+        /// </summary>
         public Color Color;
+        /// <summary>
+        /// The location (in screen coordinates) to draw the sprite.
+        /// </summary>
         public Vector2 Position;
+        /// <summary>
+        /// Effects to apply when drawing the sprite.
+        /// </summary>
         public SpriteEffects Effect;
+        /// <summary>
+        /// Specifies the angle (in radians) to rotate the sprite about its center.
+        /// </summary>
         public float Rotation;
 
+        /// <summary>
+        /// Animation used to draw the sprite.
+        /// </summary>
         public Animation CurrentAnimation { get; private set; }
+
+        /// <summary>
+        /// Indicates if the current animation is finished, or no animation is playing.
+        /// </summary>
         public bool AnimationIsFinished { get { return CurrentAnimation == null || CurrentAnimation.IsFinished; } }
 
+        /// <summary>
+        /// Creates a new sprite, containing no animations.
+        /// </summary>
         public Sprite()
         {
             _animations = new Dictionary<string, Animation>();
@@ -24,6 +50,10 @@
         }
 
         #region Game Loop
+        /// <summary>
+        /// Updates the current animation state.
+        /// </summary>
+        /// <param name="gameTime"></param>
         public void Update(GameTime gameTime)
         {
             if (CurrentAnimation == null)
@@ -31,6 +61,15 @@
             CurrentAnimation.Update(gameTime);
         }
 
+        /// <summary>
+        /// Draws the current animation frame at the current <c>Position</c>.
+        /// </summary>
+        /// <param name="spriteBatch">
+        /// Spritebatch used to draw the sprite.
+        /// <c>spritebatch.Begin()</c> and <c>spritebatch.End()</c> should be called
+        /// before and after this method call respectivelly.
+        /// </param>
+        /// <param name="gameTime">Current game time.</param>
         public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
         {
             if (CurrentAnimation == null)
@@ -40,18 +79,37 @@
         #endregion
 
         #region Public Methods
+        /// <summary>
+        /// Changes the currently playing animation.
+        /// The selected animation will be played from beggining.
+        /// If the animation is already playing nothing will be done.
+        /// </summary>
+        /// <param name="name">Name of the animation to be played.</param>
         public void PlayAnimation(string name)
         {
             if (!_animations.ContainsKey(name))
                 throw new ArgumentException("Invalid animation name", "name");
+
+            if (CurrentAnimation != null && CurrentAnimation.Name == name)
+                return;
+
             CurrentAnimation = _animations[name];
             CurrentAnimation.Reset();
         }
 
+        /// <summary>
+        /// Changes the currently playing animation.
+        /// The selected animation will be played from beggining.
+        /// If the animation is already playing nothing will be done.
+        /// </summary>
+        /// <param name="animation">The animation to be played.</param>
         public void PlayAnimation(Animation animation)
         {
             if (animation == null)
                 throw new ArgumentNullException("animation");
+
+            if (CurrentAnimation == animation)
+                return;
 
             CurrentAnimation = animation;
             CurrentAnimation.Reset();
@@ -59,6 +117,10 @@
         #endregion
 
         #region region ICollection<Animation>
+        /// <summary>
+        /// Add an animation to this sprite.
+        /// </summary>
+        /// <param name="item"><c>Jv.Games.Xna.Sprites.Animation</c> to be added.</param>
         public void Add(Animation item)
         {
             if (item == null)
@@ -71,31 +133,47 @@
                 CurrentAnimation = item;
         }
 
+        /// <summary>
+        /// Removes all registered animation.
+        /// </summary>
         public void Clear()
         {
             _animations.Clear();
         }
 
+        /// <summary>
+        /// Test if an animation is registered in this sprite.
+        /// </summary>
+        /// <param name="item">Animation to test.</param>
+        /// <returns><c>True</c> is this sprite contains the animation item.</returns>
         public bool Contains(Animation item)
         {
             return _animations.ContainsValue(item);
         }
 
-        public void CopyTo(Animation[] array, int arrayIndex)
+        void ICollection<Animation>.CopyTo(Animation[] array, int arrayIndex)
         {
             _animations.Values.CopyTo(array, arrayIndex);
         }
 
+        /// <summary>
+        /// Returns the number of animations in this <see cref="Jv.Games.Xna.Sprites.Sprite"/>.
+        /// </summary>
         public int Count
         {
             get { return _animations.Count; }
         }
 
-        public bool IsReadOnly
+        bool ICollection<Animation>.IsReadOnly
         {
             get { return false; }
         }
 
+        /// <summary>
+        /// Removes a single animation from the <see cref="Jv.Games.Xna.Sprites.Sprite"/>.
+        /// </summary>
+        /// <param name="item">Animation to be removed.</param>
+        /// <returns><c>True</c> if the animation was removed.</returns>
         public bool Remove(Animation item)
         {
             if (!_animations.ContainsValue(item))
@@ -104,6 +182,10 @@
             return _animations.Remove(item.Name);
         }
 
+        /// <summary>
+        /// Returns an enumerator that iterates through the registered animations.
+        /// </summary>
+        /// <returns></returns>
         public IEnumerator<Animation> GetEnumerator()
         {
             return _animations.Values.GetEnumerator();
--- src/Jv.Games.Xna/Jv.Games.Shared.Sprites/SpriteSheet.cs
+++ src/Jv.Games.Xna/Jv.Games.Shared.Sprites/SpriteSheet.cs
@@ -5,46 +5,36 @@
     using System;
     using System.Linq;
 
+    /// <summary>
+    /// Sprite sheet image.
+    /// </summary>
     public class SpriteSheet
     {
-        public Texture2D Texture { get; private set; }
-        public int Columns { get; private set; }
-        public int Rows { get; private set; }
-        public Point FrameSize { get; private set; }
+        /// <summary>
+        /// Sprite sheet image.
+        /// </summary>
+        public readonly Texture2D Texture;
 
-        public SpriteSheet(Texture2D texture, int columns, int rows)
+        /// <summary>
+        /// Creates a new <see cref="Jv.Games.Xna.Sprites.SpriteSheet"/>.
+        /// </summary>
+        /// <param name="texture">Texture image containing the animation frames.</param>
+        public SpriteSheet(Texture2D texture)
         {
             if (texture == null)
                 throw new ArgumentNullException("texture");
-            if (columns <= 0)
-                throw new ArgumentOutOfRangeException("columns");
-            if (rows <= 0)
-                throw new ArgumentOutOfRangeException("rows");
-
-            if (texture.Width % columns != 0 || texture.Height % rows != 0)
-                throw new ArgumentException("Texture size does not match rows / columns");
 
             Texture = texture;
-            Columns = columns;
-            Rows = rows;
-            FrameSize = new Point(texture.Width / columns, texture.Height / rows);
         }
 
-        public SpriteSheet(Texture2D texture, Point frameSize)
-        {
-            if (texture == null)
-                throw new ArgumentNullException("texture");
-            if (frameSize.X <= 0 || frameSize.Y <= 0)
-                throw new ArgumentOutOfRangeException("frameSize", "Frame size cannot be negative");
-            if (texture.Width % frameSize.X != 0 || texture.Height % frameSize.Y != 0)
-                throw new ArgumentException("Texture size does not match rows / columns");
-
-            Texture = texture;
-            Columns = texture.Width / frameSize.X;
-            Rows = texture.Height / frameSize.Y;
-            FrameSize = frameSize;
-        }
-
+        /// <summary>
+        /// Extracts a <see cref="Jv.Games.Xna.Sprites.Animation"/> with the specified frame locations.
+        /// </summary>
+        /// <param name="name">Name of the extracted animation.</param>
+        /// <param name="frameRects">Location coordinates of each frame in the animation.</param>
+        /// <param name="frameDuration">How long each frame is displayed, before switching to the next frame.</param>
+        /// <param name="repeat"><c>True</c> if the animation should repeat after the last frame finishes.</param>
+        /// <returns>The extracted animation.</returns>
         public Animation GetAnimation(string name, Rectangle[] frameRects, TimeSpan frameDuration, bool repeat = true)
         {
             if (frameRects.Length <= 0 ||
@@ -60,54 +50,6 @@
             if (repeat)
                 return new LoopedAnimation(name, frames, duration, 0, null);
             return new Animation(name, frames, duration);
-        }
-
-        public Animation GetAnimation(string name, int[] frameIndexes, TimeSpan frameDuration, bool repeat = true)
-        {
-            if (frameIndexes.Length <= 0 || frameIndexes.Any(index => index < 0 || index >= Columns * Rows))
-                throw new ArgumentOutOfRangeException("frameIndexes");
-
-            if (frameDuration <= TimeSpan.Zero)
-                throw new ArgumentOutOfRangeException("frameDuration");
-
-            var frames = frameIndexes.Select(GetFrame).ToArray();
-            var duration = TimeSpan.FromSeconds(frameDuration.TotalSeconds * frames.Length);
-            if (repeat)
-                return new LoopedAnimation(name, frames, duration, 0, null);
-            return new Animation(name, frames, duration);
-        }
-
-        public Animation GetAnimation(string name, int line, int count, TimeSpan frameDuration, bool repeat = true, int skipFrames = 0)
-        {
-            if (line < 0 || line > Rows)
-                throw new ArgumentOutOfRangeException("line");
-
-            if (count <= 0 || count > Columns)
-                throw new ArgumentOutOfRangeException("count");
-
-            if (skipFrames < 0)
-                throw new ArgumentOutOfRangeException("skipFrames");
-
-            var startIndex = (Texture.Width / FrameSize.X) * line + skipFrames;
-            var indexes = Enumerable.Range(startIndex, count - skipFrames).ToArray();
-
-            return GetAnimation(name, indexes, frameDuration, repeat);
-        }
-
-        public Frame GetFrame(int index)
-        {
-            if (index < 0 || index >= Columns * Rows)
-                throw new ArgumentOutOfRangeException("index");
-
-            return new Frame
-            (
-                Texture,
-                rectangle: new Rectangle(
-                    (index % Columns) * FrameSize.X,
-                    (index / Columns) * FrameSize.Y,
-                    FrameSize.X,
-                    FrameSize.Y)
-            );
         }
     }
 }
