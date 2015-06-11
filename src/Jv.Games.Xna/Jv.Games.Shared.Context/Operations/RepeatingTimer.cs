b89f3ff765b6bae0eeacb883a376430e67a8cbfe
Joao Vitor Pietsiaki Moraes <joao.moraes@lactec.org.br>
2015-06-11 13:07:19 -0300

    RepeatingTimer.

---
+++ src/Jv.Games.Xna/Jv.Games.Shared.Context/Operations/RepeatingTimer.cs
@@ -0,0 +1,65 @@
+﻿namespace Jv.Games.Xna.Context
+{
+    using Microsoft.Xna.Framework;
+    using System;
+
+    public class RepeatingTimer : GameOperation, IDisposable
+    {
+        private bool _disposed = false;
+
+        public TimeSpan DueTime, Period;
+
+        public event EventHandler Ellapsed = delegate { };
+
+        public RepeatingTimer(TimeSpan dueTime, TimeSpan period)
+        {
+            DueTime = dueTime;
+            Period = period;
+        }
+
+        public RepeatingTimer(TimeSpan period, bool startImmediately = true)
+        {
+            DueTime = startImmediately? TimeSpan.Zero : period;
+            Period = period;
+        }
+
+        public override bool Continue(GameTime gameTime)
+        {
+            if (_disposed)
+                return false;
+
+            DueTime -= gameTime.ElapsedGameTime;
+            if (DueTime <= TimeSpan.Zero)
+            {
+                DueTime = Period;
+                Ellapsed(this, EventArgs.Empty);
+            }
+
+            return true;
+        }
+
+        public virtual void Change(TimeSpan dueTime, TimeSpan period)
+        {
+            if (_disposed)
+                throw new ObjectDisposedException(GetType().FullName);
+
+            Period = period;
+            DueTime = dueTime;
+        }
+
+        public void Dispose()
+        {
+            _disposed = true;
+        }
+    }
+
+    public static class RepeatingTimerExtensions
+    {
+        public static RepeatingTimer StartTimer(this IContext context, TimeSpan period, bool startImmediatelly = true)
+        {
+            var timer = new RepeatingTimer(period, startImmediatelly);
+            context.Run(timer);
+            return timer;
+        }
+    }
+}
